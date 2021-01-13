dff0a34a775921978f535c513db6388f8dec81dc
Tim Seryakov <t.seryakov@gmail.com>
2021-01-13 14:14:43 +0300

    router and page 404

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import {Redirect, Route, Switch } from 'react-router-dom'
+import {Page404} from "./components/Page404/Page404";
 
 export const App = () => {
     return (
         <div>
             <h1 className="text-blue-800 text-2xl">Hello</h1>
+            <Switch>
+                {/*<Route exact path={'/'} render={ }/>*/}
+                <Route path={"/404"} render={ () => <Page404/> }/>
+                <Redirect from= '*' to="/404"/>
+            </Switch>
         </div>
     )
 }
---
+++ src/components/Page404/Page404.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import cat from "./cat404.svg"
+
+export const Page404 = () => {
+    return (
+        <div className="flex items-center justify-center w-full min-h-screen">
+            <div className="flex flex-col text-gray-700 lg:flex-row lg:space-x-16 lg:space-x-reverse">
+                <div className="order-1 max-w-md px-2 text-sm md:text-base lg:px-0">
+                    <header className="mb-6">
+                        <h2 className="text-4xl font-bold leading-none text-gray-400 select-none lg:text-6xl">404.</h2>
+                        <h3 className="text-xl font-light leading-normal lg:text-3xl md:text-3xl">
+                            Sorry, we couldn't find this page.
+                        </h3>
+                    </header>
+
+                    <p className="max-w-sm mb-5 leading-5 md:leading-7">
+                        Don't worry, sometimes even we make mistakes.
+                        You can find plenty of other things on our homepage.
+                    </p>
+
+                    <a href="/"
+                        className="inline px-4 py-2 text-sm font-medium leading-5 text-white uppercase transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg shadow focus:outline-none focus:shadow-outline-blue active:bg-blue-600 hover:bg-blue-700">
+                        Back to Homepage
+                    </a>
+                </div>
+
+                <div className="max-w-lg">
+                    <img src={cat} alt="Page not found. Meow."/>
+                </div>
+
+            </div>
+        </div>
+    )
+}
